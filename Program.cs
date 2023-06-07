95e2c307bf86006b8322512ee8e8bf1269fc14ca
Chandrakant Gurhale <chandrakant-gurhale-contractor-astellas-com>
2023-06-07 18:02:45 +0530

    Hello World added

--- Program.cs
+++ Program.cs
@@ -1,26 +1,3 @@
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-
-namespace HelloWeb
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-
-            var config = new ConfigurationBuilder()
-                          .AddCommandLine(args)
-                          .Build();
-            var host = new WebHostBuilder()
-                        .UseKestrel()
-                        .UseConfiguration(config)
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseIISIntegration()
-                        .UseStartup<Startup>()
-                        .Build();
-
-            host.Run();
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
